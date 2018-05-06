41e4e51a4663c13858f8cfc5c0eae5a766e0d8f7
Vitaly Gontarenko <vitalygontarenko@yandex.ru>
2018-05-07 01:33:15 +0300

    finish all section without scrollbar, upbutton, include bold font, smooth scroll, mobile view

--- js/common.js
+++ js/common.js
@@ -3,7 +3,18 @@ $(document).ready(function(){
         items: 1,
         loop: true,
         nav: true,
-
     });
+
+    // добавление имя фотографии в форму
+    $('#photo').change(function(){
+        var file = $(this).val().split('\\');
+        var fileName = file[file.length - 1];
+        $(this).parent().find('.photo-name').html(fileName);
+    });
+    //убираем событие при клике на имя фото
+    $('.photo-name').click(function () {
+        return false;
+    });
+
 
 });
