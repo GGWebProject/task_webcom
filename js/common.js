fca85dc4329f029387d39adbf8bdcfcc770f65e8
Vitaly Gontarenko <vitalygontarenko@yandex.ru>
2018-05-08 00:49:18 +0300

    +scroll menu, all layout finish, without logic js

--- js/common.js
+++ js/common.js
@@ -11,10 +11,42 @@ $(document).ready(function(){
         var fileName = file[file.length - 1];
         $(this).parent().find('.photo-name').html(fileName);
     });
-    //убираем событие при клике на имя фото
-    $('.photo-name').click(function () {
-        return false;
-    });
+
+});
+
+function chengeHeader() {
+    if ($(window).scrollTop() > 0) {
+        $('header').addClass('header-scroll');
+        $('main').addClass('main-scroll');
+    } else {
+        $('header').removeClass('header-scroll');
+        $('main').removeClass('main-scroll');
+    }
+}
 
 
+// var sectionArray = $('main');
+// console.log($('main').children().eq(2));
+// console.log($('main').children().eq(2).position().top);
+// console.log($('main').children().eq(2).outerHeight());
+// console.log($('main').children().eq(2).offset());
+// console.log($(window).scrollTop());
+// scrollBar();
+//
+// function scrollBar() {
+//     console.log(sectionArray);
+//
+// }
+
+var countSec = $('main').prop('childElementCount');
+console.log(countSec);
+for(var i = 0; i <= countSec; i++){
+    console.log($('main').children().eq(i));
+    console.log(i);
+}
+
+
+$(window).on("scroll", function() {
+    chengeHeader();
+    console.log($(window).scrollTop());
 });
